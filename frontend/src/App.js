835727b444650d763700d2f10b209e4d196363d9
arjunp8822 <106944140+arjunp8822@users.noreply.github.com>
2023-06-05 17:33:45 +1000

    added edit and delete forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,27 @@ function App() {
   const [newCategory, setNewCategory] = useState(null)
   const [newDescription, setNewDescription] = useState(null)
   const [displayCreate, setDisplayCreate] = useState(false)
+  const [displayTodo, setDisplayTodo] = useState(false)
+  const [updateTask, setUpdateTask] = useState(null)
+  const [updateStatus, setUpdateStatus] = useState(null)
+  const [updateCategory, setUpdateCategory] = useState(null)
+  const [updateDescription, setUpdateDescription] = useState(null)
+
+  useEffect(() => {
+    setLoading(true)
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/api/todos')
+      if (response) {
+        const jsonData = await response.json()
+        setData(jsonData)
+        setLoading(false)
+      } else {
+        throw new Error('Please try again later')
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
 
   const submitHandler = async (e) => {
     e.preventDefault()
@@ -36,21 +57,16 @@ function App() {
     window.location.reload(false);
   }
 
-  useEffect(() => {
-    setLoading(true)
-    const fetchData = async () => {
-      const response = await fetch('http://localhost:5000/api/todos')
-      if (response) {
-        const jsonData = await response.json()
-        setData(jsonData)
-        setLoading(false)
-      } else {
-        throw new Error('Please try again later')
-        setLoading(false)
-      }
-    }
-    fetchData()
-  }, [])
+  const getHandler = async (e) => {
+    const id = e.currentTarget.id
+    const post = await fetch(`http://localhost:5000/api/todos/${id}`)
+    const response = await post.json()
+    setDisplayTodo(true)
+    setUpdateTask(response.task)
+    setUpdateStatus(response.status)
+    setUpdateCategory(response.category)
+    setUpdateDescription(response.description)
+  }
 
   const deleteHandler = async (e) => {
     try {
@@ -65,10 +81,14 @@ function App() {
   }
 
   const updateHandler = async (e) => {
+    e.preventDefault()
     try {
       const id = await e.target.value
       const updatedTodo = {
-        'task': 'test update'
+        'task': updateTask,
+        'status': updateStatus,
+        'category': updateCategory,
+        'description': updateDescription
       }
       const updatedPost = await fetch(`http://localhost:5000/api/todos/${id}`, {
         method: 'PUT',
@@ -96,8 +116,8 @@ function App() {
             <AiOutlinePlus onClick={() => setDisplayCreate(true)} />
           </nav>
           {data && data.map((item) => (
-            <div>
-              <Todo task={item.task} />
+            <div onClick={getHandler} id={item._id} >
+              <Todo task={item.task} id={item._id} />
             </div>
           ))}
         </div>
@@ -119,6 +139,7 @@ function App() {
               <div className='create-status-buttons'>
                 <button onClick={() => setNewStatus(1)} type='button' className={newStatus === 1 ? 'button-active' : 'button-inactive'}>Not Started</button>
                 <button onClick={() => setNewStatus(2)} type='button' className={newStatus === 2 ? 'button-active' : 'button-inactive'}>Started</button>
+                <button onClick={() => setNewStatus(3)} type='button' className={newStatus === 3 ? 'button-active' : 'button-inactive'}>Complete</button>
               </div>
             </div>
             <textarea placeholder='Description' type='text' onChange={(e) => setNewDescription(e.target.value)} className='new-description' />
@@ -127,6 +148,35 @@ function App() {
             </div>
           </form>
         </div>
+
+        {/* todo edit modal */}
+
+        <div className={displayTodo ? 'create-modal' : 'create-modal-inactive'}>
+          <form onSubmit={updateHandler} className='create-container'>
+            <div className='create-container-close-container'>
+              <AiFillCloseCircle className='create-container-close-button' onClick={() => setDisplayTodo(false)} />
+            </div>
+            <input value={updateTask} type='text' onChange={(e) => setUpdateTask(e.target.value)} className='new-task' />
+            <div className='create-buttons'>
+              <div className='create-category-buttons'>
+                <button onClick={() => setUpdateCategory('Personal')} type='button' className={updateCategory === 'Personal' ? 'button-active' : 'button-inactive'}>Personal</button>
+                <button onClick={() => setUpdateCategory('Study')} type='button' className={updateCategory === 'Study' ? 'button-active' : 'button-inactive'}>Study</button>
+                <button onClick={() => setUpdateCategory('Work')} type='button' className={updateCategory === 'Work' ? 'button-active' : 'button-inactive'}>Work</button>
+              </div>
+              <div className='create-status-buttons'>
+                <button onClick={() => setUpdateStatus(1)} type='button' className={updateStatus === 1 ? 'button-active' : 'button-inactive'}>Not Started</button>
+                <button onClick={() => setUpdateStatus(2)} type='button' className={updateStatus === 2 ? 'button-active' : 'button-inactive'}>Started</button>
+                <button onClick={() => setUpdateStatus(3)} type='button' className={updateStatus === 3 ? 'button-active' : 'button-inactive'}>Complete</button>
+              </div>
+            </div>
+            <textarea value={updateDescription} type='text' onChange={(e) => setUpdateDescription(e.target.value)} className='new-description' />
+            <div className='create-submit-button-container'>
+              <button type='submit'>Update</button>
+              <button type='submit' onClick={deleteHandler}>Delete</button>
+            </div>
+          </form>
+        </div>
+
       </>
     );
   }
